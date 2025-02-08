7d7461d1cfc9a487c2f1f7651cd55ea69f1f422c
b3nparker <b1c1parker@comcast.net>
2025-02-08 08:07:37 -0500

    More pinata

---
+++ frontend/src/app/components/upload.tsx
@@ -0,0 +1,10 @@
+"use client";
+import "../globals.css";
+import { Monoton } from "next/font/google";
+import { useState } from "react";
+
+const monoton = Monoton({
+  weight: "400",
+  subsets: ["latin"],
+});
+
