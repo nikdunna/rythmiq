4ed2889e88e0f47dccb81d7e2e3f5dc2838ee70f
b3nparker <b1c1parker@comcast.net>
2025-02-08 03:31:21 -0500

    Added rudimentary pinata function.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "./components/hero";
 import Studio from "./components/studio";
 import { useState } from "react";
 import Generator from "./components/generator";
+import { pinata } from "@/app/utils/config";
+
 export default function Home() {
   const [showStudio, setShowStudio] = useState(false);
   const [showGenerator, setShowGenerator] = useState(false);
@@ -19,11 +21,48 @@ export default function Home() {
     console.log("Audio recording completed:", audioUrl);
   };
 
+  const [file, setFile] = useState<File>();
+	const [url, setUrl] = useState("");
+	const [uploading, setUploading] = useState(false);
+
+	const uploadFile = async () => {
+		if (!file) {
+			alert("No file selected");
+			return;
+		}
+
+		try {
+			setUploading(true);
+			const keyRequest = await fetch("/api/key");
+			const keyData = await keyRequest.json();
+			const upload = await pinata.upload.file(file).key(keyData.JWT);
+			const ipfsUrl = await pinata.gateways.convert(upload.IpfsHash)
+			setUrl(ipfsUrl);
+			setUploading(false);
+		} catch (e) {
+			console.log(e);
+			setUploading(false);
+			alert("Trouble uploading file");
+		}
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFile(e.target?.files?.[0]);
+	};
+
+
   return (
     <div className="bg-black h-screen">
       <Hero onEnterStudio={handleEnterStudio} />
       <Studio show={showStudio} onAudioComplete={handleAudioComplete} />
       <Generator audioURL={recordedAudioUrl} show={showGenerator} />
+      
+      <input type="file" onChange={handleChange} />
+			<button type="button" disabled={uploading} onClick={uploadFile}>
+				{uploading ? "Uploading..." : "Upload"}
+			</button>
+			{url && <img src={url} alt="Image from Pinata" />}
+
     </div>
   );
 }
